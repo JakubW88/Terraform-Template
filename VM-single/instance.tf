b91c5dce9adf8464102109e08b665b69b181ebc2
Jacob from Bialystok ! <32395273+JakubW88@users.noreply.github.com>
2018-10-16 09:51:36 +0200

    16.10 update poranny

---
+++ VM-single/instance.tf
@@ -0,0 +1,18 @@
+resource "google_compute_instance" "default" {
+  project      = "terraform-219511"
+  name         = "terraform01"
+  machine_type = "n1-standard-1"
+  zone         = "europe-west3-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
